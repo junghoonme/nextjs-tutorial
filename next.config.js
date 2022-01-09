1d5c40a97cf51f6b9c3be8fe9e3c531d81132a10
junghoonme <junghoon.dev@gmail.com>
2022-01-09 20:34:23 +0900

    Add Static Generation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env: { BASE_URL: process.env.BASE_URL },
   images: {
     domains: ['d3t32hsnjxo7q6.cloudfront.net'],
   },
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,38 @@
 import Head from 'next/head';
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ItemList from '../src/components/ItemList';
 import { Divider, Header } from 'semantic-ui-react';
-import Loading from '../src/components/Loading';
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  useEffect(() => {
-    setIsLoading(true);
-    axios.get(API_URL).then((res) => {
-      setList(res.data);
-      setIsLoading(false);
-    });
-  }, [API_URL]);
-
+export default function Home({ list }) {
   return (
     <div>
       <Head>
         <title>Next.js Tutorial</title>
         <meta name='description' content='홈커밍'></meta>
       </Head>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <Header as='h3' style={{ paddingTop: 40 }}>
-            베스트 상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(0, 9)} />
-          <Header as='h3' style={{ paddingTop: 40 }}>
-            신상품
-          </Header>
-          <Divider />
-          <ItemList list={list.slice(9)} />
-        </>
-      )}
+      <Header as='h3' style={{ paddingTop: 40 }}>
+        베스트 상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(0, 9)} />
+      <Header as='h3' style={{ paddingTop: 40 }}>
+        신상품
+      </Header>
+      <Divider />
+      <ItemList list={list.slice(9)} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
